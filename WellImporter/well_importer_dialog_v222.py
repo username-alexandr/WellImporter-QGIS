dd949dcee7d6381c780704c91aa4443d55963be0
Alexandr <56488397+SashaRai@users.noreply.github.com>
2026-08-14 08:35:37 +0300

    fix: activate robust CSV decoder in main dialog

--- WellImporter/well_importer_dialog_v222.py
+++ WellImporter/well_importer_dialog_v222.py
@@ -2,11 +2,13 @@
 
 from .spatial_circle_repair import SpatialCircleRepairManager
 from .well_importer_dialog import WellImporterDialog
+from .robust_file_importer import RobustExcelFileImporter
 
 
 class WellImporterDialogV222(WellImporterDialog):
-    """Главное окно с исправленной пространственной достройкой кругов."""
+    """Главное окно с исправленной достройкой кругов и устойчивым CSV/TXT-декодером."""
 
     def __init__(self, iface, parent=None):
         super().__init__(iface, parent)
         self.controller.circle_repair = SpatialCircleRepairManager()
+        self.file_importer = RobustExcelFileImporter()
