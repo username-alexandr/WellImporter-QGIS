8c0428f5ef3beb87a5609ed329e7b335fe628ca9
Alexandr <56488397+username-alexandr@users.noreply.github.com>
2026-08-19 12:47:30 +0300

    fix: activate 2.2.4 runtime interface

--- WellImporter/__init__.py
+++ WellImporter/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
 
 def classFactory(iface):
-    from .well_importer_v22_interface import WellImporterV22Interface
-    return WellImporterV22Interface(iface)
+    from .well_importer_v224_interface import WellImporterV224Interface
+    return WellImporterV224Interface(iface)
